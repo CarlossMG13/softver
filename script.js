4906bd87c246f3f43f5dcdf60fc914a0aa56cef7
Carlos Morales García <116101975+CarlossMG13@users.noreply.github.com>
2025-03-26 12:09:36 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 AOS.init();
+
+//buttons
+
+/* Log-in */
+document.getElementById('log-in').addEventListener('click', function(){
+    window.location.href = 'http://logistic.softver.mx/vista/login/login.html';
+})
+
+/* products to landing page */
+document.getElementById('products').addEventListener('click', function(){
+    window.location.href = 'https://softver.mx/logistics/';
+})
